9385e7414606b947d5e295f3bed67e83db512c8a
Mateo2208 <mateoandres2208@gmail.com>
2025-05-14 21:56:24 -0400

    Me olvide esto

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def serve_frontend(path):
         return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
